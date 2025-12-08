b1b89b351cf83c9322d484df99f80bb3bf0277f9
Guilherme Moneda <guimoneda@gmail.com>
2025-12-07 22:43:39 -0500

    Update tables

--- backend/resume/models.py
+++ backend/resume/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    company = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True) # Null means "Present"
+    description = models.TextField()
+    is_current = models.BooleanField(default=False)
+    
+    # This makes it sort automatically by newest first
+    class Meta:
+        ordering = ['-start_date']
+
+    def __str__(self):
+        return f"{self.title} at {self.company}"
