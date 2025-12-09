e9c6b2b7cf15ba76e331b31b1809753da6180185
Guilherme Moneda <guimoneda@gmail.com>
2025-12-08 19:30:55 -0500

    Create backend api for jobs

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'resume',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Allow React to talk to Django
+CORS_ALLOWED_ORIGINS = [
+    "https://bio.guimoneda.com",
+    "http://localhost:3000",
+]
--- backend/resume/views.py
+++ backend/resume/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .models import Job
+from .serializers import JobSerializer
+
+class JobList(generics.ListAPIView):
+    # Get all jobs, sorted by start date (defined in model)
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
