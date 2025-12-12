1a9f3e5d215b72aa99af783c5352b920b547dbe4
Guilherme Moneda <guimoneda@gmail.com>
2025-12-12 00:02:25 -0500

    Fixed variable typo

--- frontend/src/components/EducationList.tsx
+++ frontend/src/components/EducationList.tsx
@@ -51,9 +51,9 @@ const EducationList: React.FC = () => {
                 </div>
             
                 <div className="grid grid-cols-[auto_20px_auto] gap-1 text-sm text-gray-500 font-mono">
-                  <span className="text-right">{item.startDate}</span>
+                  <span className="text-right">{school.startDate}</span>
                   <span className="text-center">&rarr;</span>
-                  <span className="text-left w-[80px]">{item.endDate}</span> 
+                  <span className="text-left w-[80px]">{school.endDate}</span> 
                   {/* w-[80px] ensures 'Present' takes up same space as a date */}
                 </div>
             </div>
