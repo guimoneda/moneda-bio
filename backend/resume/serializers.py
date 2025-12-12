6e54d83122c0feb22a75fe7d321ab5686f085e01
Guilherme Moneda <guimoneda@gmail.com>
2025-12-11 23:09:31 -0500

    Fixed education type

--- backend/resume/serializers.py
+++ backend/resume/serializers.py
@@ -4,10 +4,10 @@ from .models import Job, Education
 class JobSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
-        fields = ['id', 'company', 'title', 'start_date', 'end_date', 'description', 'technologies', 'image', 'image_url', 'is_current',]
+        fields = ['id', 'company', 'title', 'start_date', 'end_date', 'description', 'technologies', 'image', 'image_url', 'is_current']
 
 
 class EducationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Education
-        fields = '__all__'
+        fields = ['degree', 'institution', 'start_date', 'end_date', 'is_active']
